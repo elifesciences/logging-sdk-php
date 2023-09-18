2f3ceb10d0adf5ecf5b0f1df50cda8e457236bc0
Luke <2142748+lsh-0@users.noreply.github.com>
2023-09-18 16:13:14 +0930

    linted

--- src/Monitoring.php
+++ src/Monitoring.php
@@ -9,31 +9,25 @@ final class Monitoring
 {
     public function __construct()
     {
-
     }
 
     public function markAsBackground()
     {
-
     }
 
     public function nameTransaction($name)
     {
-
     }
 
     public function startTransaction()
     {
-
     }
 
     public function endTransaction()
     {
-
     }
 
     public function recordException(Throwable $exception, $message = null)
     {
-
     }
 }
